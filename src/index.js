141b49069a4cde2aa725257aa6d645fc025e001e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-12-29 12:53:08 +0100

    Add menu-page/images imports and nav buttons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import initPage from './modules/webpage.js';
 import createHome from './modules/home.js';
+import createMenu from './modules/menu.js';
 import './styles.css';
 
-initPage();
-createHome();
+function importAll(r) {
+    r.keys().forEach(r)
+}
+importAll(require.context('/src/images', false, /\.jpg$/));
+
+function addButtons() {
+    const homeBtn = document.getElementById("home");
+    const menuBtn = document.getElementById("menu");
+    // const contactBtn = document.getElementById("contact");
+
+    homeBtn.addEventListener("click", createHome);
+    menuBtn.addEventListener("click", createMenu);
+    // contactBtn.addEventListener("click", createContact);
+}
+
+(function loadPage() {
+    initPage();
+    createHome();
+    addButtons();
+})();
